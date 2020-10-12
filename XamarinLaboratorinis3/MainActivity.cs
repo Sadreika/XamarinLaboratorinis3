cd2844490e1870735e87d626c05ca69865c5ee64
MrMariu5 <marius.sadreika@gmail.com>
2020-10-12 19:24:38 +0300

    80 proc done

--- XamarinLaboratorinis3/MainActivity.cs
+++ XamarinLaboratorinis3/MainActivity.cs
@@ -3,6 +3,8 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Java.Lang;
+using Java.Util;
 
 namespace XamarinLaboratorinis3
 {
@@ -13,9 +15,30 @@ namespace XamarinLaboratorinis3
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            AutoCompleteTextView autoCompleteTextView = FindViewById<AutoCompleteTextView>(Resource.Id.fakultetasInput);
+            var autoCompleteOptions = new string[] { "Fundamentinių mokslų fakultetas" };
+            ArrayAdapter autoCompleteAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line, autoCompleteOptions);
+            autoCompleteTextView.Adapter = autoCompleteAdapter;
+            Button button = FindViewById<Button>(Resource.Id.saugojimas);
+            button.Click += Button_Click;
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.dienosInput);
+            var spinnerOptions = new string[] { "Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis", "Sekmadienis" };
+            ArrayAdapter spinnerOptionsAdapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleDropDownItem1Line, spinnerOptions);
+            spinner.Adapter = spinnerOptionsAdapter;
+
+            TimePicker timePicker = FindViewById<TimePicker>(Resource.Id.laikasInput);
+            timePicker.SetIs24HourView(Java.Lang.Boolean.False);
+          
         }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "Sup", ToastLength.Short).Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
